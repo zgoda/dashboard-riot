9df99a1783a80a463fc3af06f380b85e577a2396
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-04 20:33:40 +0100

    Rollup & eslint configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import riot from 'rollup-plugin-riot'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser'
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default {
   input: 'src/index.js',
